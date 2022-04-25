8c4dc53301dbc52f18a7c247f4034f3931c5741a
Lewis Hannah <70963125+ljhannah74@users.noreply.github.com>
2022-04-25 11:09:27 -0400

    Update t_resource.cs

--- Orb.Biz.OnlineResources/t_resource.cs
+++ Orb.Biz.OnlineResources/t_resource.cs
@@ -21,49 +21,49 @@ namespace Orb.Biz.OnlineResources
 		public string county { get; set; }
 		
 		[Column(DbType = "bit", CanBeNull = true, Storage = "ins")]
-		public string ins { get; set; }
+		public bool ins { get; set; }
 
 		[Column(DbType = "bit", CanBeNull = true, Storage = "props")]
-		public string props { get; set; }
+		public bool props { get; set; }
 
 		[Column(DbType = "bit", CanBeNull = true, Storage = "doc_retrieval")]
-		public string doc_retrieval { get; set; }
+		public bool doc_retrieval { get; set; }
 
 		[Column(DbType = "money", CanBeNull = true, Storage = "subscrFeeAmt")]
-		public string subscrFeeAmt { get; set; }
+		public decimal subscrFeeAmt { get; set; }
 
 		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "subscr_term")]
 		public string subscr_term { get; set; }
 
 		[Column(DbType = "bit", CanBeNull = true, Storage = "subscribed")]
-		public string subscribed { get; set; }
+		public bool subscribed { get; set; }
 
 		[Column(DbType = "bit", CanBeNull = true, Storage = "sub_need")]
-		public string sub_need { get; set; }
+		public bool sub_need { get; set; }
 
 		[Column(DbType = "bit", CanBeNull = true, Storage = "tap")]
-		public string tap { get; set; }
+		public bool tap { get; set; }
 
 		[Column(DbType = "bit", CanBeNull = true, Storage = "rv")]
-		public string rv { get; set; }
+		public bool rv { get; set; }
 
 		[Column(DbType = "bit", CanBeNull = true, Storage = "dtree_desk")]
-		public string dtree_desk { get; set; }
+		public bool dtree_desk { get; set; }
 
 		[Column(DbType = "date", CanBeNull = true, Storage = "img_date")]
-		public string img_date { get; set; }
+		public DateTime img_date { get; set; }
 
 		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "copy_source")]
 		public string copy_source { get; set; }
 
 		[Column(DbType = "money", CanBeNull = true, Storage = "copyFeeAmt")]
-		public string copyFeeAmt { get; set; }
+		public decimal copyFeeAmt { get; set; }
 
 		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "copy_pmt_method")]
 		public string copy_pmt_method { get; set; }
 
 		[Column(DbType = "date", CanBeNull = true, Storage = "index_date")]
-		public string index_date { get; set; }
+		public DateTime index_date { get; set; }
 
 		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "index_source")]
 		public string index_source { get; set; }
@@ -81,72 +81,105 @@ namespace Orb.Biz.OnlineResources
 		public string county_pwd { get; set; }
 
 		[Column(DbType = "bit NOT NULL", Storage = "login_req", CanBeNull = false]
-		public string login_required { get; set; }
+		public bool login_req { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "court_url")]
 		public string court_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "court_user")]
 		public string court_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "court_pwd")]
 		public string court_pwd { get; set; }
 
-		public string courtIndexDate { get; set; }
+		[Column(DbType = "date", CanBeNull = true, Storage = "courtIndexDate")]
+		public DateTime courtIndexDate { get; set; }
 
-		public string courtImageDate { get; set; }
+		[Column(DbType = "date", CanBeNull = true, Storage = "courtImageDate")]
+		public DateTime courtImageDate { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "muniCourt_url")]
 		public string muniCourt_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "muniCourt_user")]
 		public string muniCourt_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "muniCourt_pwd")]
 		public string muniCourt_pwd { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "tax_url")]
 		public string tax_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "tax_user")]
 		public string tax_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "tax_pwd")]
 		public string tax_pwd { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "tax2_url")]
 		public string tax2_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "tax2_user")]
 		public string tax2_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "tax2_pwd")]
 		public string tax2_pwd { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "prothon_url")]
 		public string prothon_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "pro_user")]
 		public string pro_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "pro_pwd")]
 		public string pro_pwd { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "assessor_url")]
 		public string assessor_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "assessor_user")]
 		public string assessor_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "assessor_pwd")]
 		public string assessor_pwd { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "probate_url")]
 		public string probate_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "probate_user")]
 		public string probate_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "probate_pwd")]
 		public string probate_pwd { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "other_url")]
 		public string other_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "other_user")]
 		public string other_user { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "other_pwd")]
 		public string other_pwd { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "ucc_url")]
 		public string ucc_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "map_url")]
 		public string map_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "plat_url")]
 		public string plat_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "foreclosure_url")]
 		public string foreclosure_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "sheriff_url")]
 		public string sheriff_url { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "county_homepage")]
 		public string county_homepage { get; set; }
 
+		[Column(DbType = "NVarChar(255)", CanBeNull = true, Storage = "comments")]
 		public string comments { get; set; }
 	}
 }
